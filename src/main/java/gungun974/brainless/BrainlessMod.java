f8a135263b69facdd11acac2c7e5886a0d22ecbe
Gungun974 <xfelix974@gmail.com>
2025-06-21 23:20:52 +0400

    Fix build

--- src/main/java/gungun974/brainless/BrainlessMod.java
+++ src/main/java/gungun974/brainless/BrainlessMod.java
@@ -3,35 +3,13 @@ package gungun974.brainless;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.util.GameStartEntrypoint;
-import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class BrainlessMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+public class BrainlessMod implements ModInitializer {
     public static final String MOD_ID = "TheyStoleMaxBrain";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
         LOGGER.info("TheyStoleMaxBrain initialized.");
     }
-
-	@Override
-	public void onRecipesReady() {
-
-	}
-
-	@Override
-	public void initNamespaces() {
-
-	}
-
-	@Override
-	public void beforeGameStart() {
-
-	}
-
-	@Override
-	public void afterGameStart() {
-
-	}
 }
